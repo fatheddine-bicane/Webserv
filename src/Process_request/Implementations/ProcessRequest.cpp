3a641bcb7ec372122c6a0f370b804c87ef715fa0
Narancia34 <tahagamraoui@gmail.com>
2026-06-27 18:11:59 +0100

    feat(processing): add processPostRequest for basic file uploads and validation

--- src/Process_request/Implementations/ProcessRequest.cpp
+++ src/Process_request/Implementations/ProcessRequest.cpp
@@ -2,6 +2,7 @@
 #include "../Exceptions/ProcessRequestException.hpp"
 #include <cstddef>
 #include <list>
+#include <sys/stat.h>
 
 
 // INFO: constructor
@@ -25,7 +26,7 @@ void	ProcessRequest::processRequest() {
 	if (this->_request.isRequestState(MALFORMED)) {
 		// handle error
 	}
-
+	
 	else {
 		try {
 			switch (this->_request.method) {
@@ -35,6 +36,8 @@ void	ProcessRequest::processRequest() {
 
 				case POST:
 					// handle post
+					findLocationBlock();
+					processPostRequest();
 					break;
 
 				case DELETE:
@@ -49,7 +52,7 @@ void	ProcessRequest::processRequest() {
 
 		// handler function couldnt process the request
 		catch (ProcessRequestException& e) {
-			// handl error
+			std::cout << "ProcessRequestException: " << e.what() << std::endl;
 		}
 	}
 }
@@ -98,4 +101,54 @@ void	ProcessRequest::findLocationBlock() {
 			throw ProcessRequestException(NotFound);
 		}
 	}
+}
+
+static String getMethodName(HTTPMethod method) {
+	switch (method) {
+		case GET: return "GET";
+		case POST: return "POST";
+		case DELETE: return "DELETE";
+		case PUT: return "PUT";
+		default: return "";
+	}
+}
+
+void ProcessRequest::processPostRequest(){
+	// 1 check if POST is allowed in _location
+	if (!this->_location->limit_except.empty()) {
+		String methodName = getMethodName(this->_request.method);
+		if (this->_location->limit_except.find(methodName) == this->_location->limit_except.end()) {
+			throw ProcessRequestException(MethodNotAllowed);
+		}
+	}
+	// 2 check if location is for CGI
+	// if cgi ...
+	// else
+
+	// 3 Handle as upload
+	String root = this->_location->shared_directives.root;
+	if (!root.empty() && root[root.length() - 1] == '/') {
+		root.erase(root.length() - 1);
+	}
+	String filePath = root + this->_request.target_resource;
+
+	// Open the temporary file where the request body was stored
+	std::ifstream inFile(this->_request.tmp_body_file_name.c_str(), std::ios::binary);
+	if (!inFile) {
+		throw ProcessRequestException(InternalServerError);
+	}
+	// Open the destination file
+	std::ofstream outFile(("./nginx/" + filePath).c_str(), std::ios::binary);
+	if (!outFile) {
+		inFile.close();
+		throw ProcessRequestException(InternalServerError);
+	}
+	// Write the contents from the temporary file to the destination file
+	outFile << inFile.rdbuf();
+
+	inFile.close();
+	outFile.close();
+
+	remove(this->_request.tmp_body_file_name.c_str());
+
 }
