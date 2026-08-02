35e99b37463d8fb129c77caca4a27c5aa7b59c0e
fatheddine-bicane <fatheddine04@gmail.com>
2026-08-02 22:04:11 +0100

    feat: method to start monitoring the socket for output

--- src/Core_modules/Connection/Definitions/ClientConnection.hpp
+++ src/Core_modules/Connection/Definitions/ClientConnection.hpp
@@ -2,7 +2,11 @@
 
 #include "Connection.hpp"
 #include "../../../HTTP_request_parser/Definitions/Request.hpp"
+#include "../../../Responce_builder/Definitions/Responce.hpp"
+#include "../../../Server_multiplexing/Exceptions/SystemCallsExceptions.hpp"
 
+
+// class Responce;
 
 enum ConnectionState {
 	KEEP_ALIVE, CLOSE
@@ -15,7 +19,10 @@ public:
 	Server*			server;
 	String&	ip_port;
 	Servers&	servers;
+	Responce	responce;
 
 public:
 	ClientConnection(SOCKET fd, String& ip_port, Servers& servers);
+
+	void	monitorSockerForOutput(EP_INSTANCE ep_instance);
 };
--- src/Core_modules/Connection/Implementations/ClientConnection.cpp
+++ src/Core_modules/Connection/Implementations/ClientConnection.cpp
@@ -1,4 +1,8 @@
+#include <sys/epoll.h>
+
 #include "../Definitions/ClientConnection.hpp"
+#include "../../../Responce_builder/Definitions/Responce.hpp"
+#include "../../Typedef.hpp"
 
 ClientConnection::ClientConnection(SOCKET fd,
 								   String& ip_port,
@@ -6,6 +10,21 @@ ClientConnection::ClientConnection(SOCKET fd,
 	: Connection(CLIENT_S, fd),
 	  request(fd, this),
 	  ip_port(ip_port),
-	  servers(servers) {
+	  servers(servers),
+	  responce(this) {
 	this->server = NULL;
+}
+
+
+
+void	ClientConnection::monitorSockerForOutput(EP_INSTANCE ep_instance) {
+	struct epoll_event event;
+
+	// monitor socket for output
+    event.events = EPOLLOUT | EPOLLET;
+    event.data.ptr = this;
+
+    if (epoll_ctl(ep_instance, EPOLL_CTL_MOD, this->fd, &event) == -1) {
+        throw SystemCallsFailedException("epoll_ctl()");
+    }
 }
